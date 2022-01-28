17f82658645615831082967529e5f9a1c42f2911
H-M-Masud <hmmasud163@gmail.com>
2022-01-29 00:13:31 +0600

    added object

---
+++ object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id:115,
+    name: 'Abir Adnan',
+    marks: 81,
+    phone:015556
+};
+
+var mobile = {
+    color: 'black',
+    price: 19000,
+    screenSize: 5,
+    storage: '16gb'
+};
+
+var book = {
+    title: 'Start with why',
+    author: 'simon sinek',
+    price: 200,
+    pages: 246,
+    publisher: 'penguin'
+}
