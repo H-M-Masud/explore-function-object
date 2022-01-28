6da81afe6218e2b3f1ff2309ed6dcdc060fe2573
H-M-Masud <hmmasud163@gmail.com>
2022-01-29 00:06:54 +0600

    added multiple parameter

---
+++ multiple-parameter.js
@@ -0,0 +1,41 @@
+// function add numbers
+
+function addTwoNumbers(number1, number2){
+    var total = number1 + number2;
+    return total;
+}
+
+var firstNumber = 32;
+var secondNumber = 45;
+var result = addTwoNumbers(firstNumber, secondNumber);
+console.log(result)
+
+
+// function multiple number
+
+function multiplyTwoNumbers(num1, num2){
+    var result = num1 * num2;
+    return result;
+}
+
+var total = multiplyTwoNumbers(5, 100);
+console.log(total)
+
+// find out even number with function
+
+function evenTwoNumbers(first, second){
+    var totalMultiple = first * second;
+    return totalMultiple;
+}
+
+var result = evenTwoNumbers(15, 50);
+console.log(result)
+
+// find out division with function
+function divisionTwoNumbers(num1, num2){
+    var totalDivision = num1 / num2;
+    return totalDivision;
+}
+
+var result = divisionTwoNumbers(1500, 50);
+console.log(result)
