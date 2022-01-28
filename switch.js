720e65c57c2163fc1a280bce60c3d28e05934dd3
H-M-Masud <hmmasud163@gmail.com>
2022-01-29 00:39:21 +0600

    added switch

---
+++ switch.js
@@ -0,0 +1,44 @@
+// use of if, else if, else
+
+var color = 'white';
+
+if(color == 'blue'){
+    console.log('color is blue');
+}
+else if(color == 'red'){
+    console.log('color is red');
+}
+else if(color == 'green'){
+    console.log('color is green');
+}
+else if(color == 'magenta'){
+    console.log('color is magenta');
+}
+else if(color == 'white'){
+    console.log('color is white');
+}
+else if(color == 'yellow'){
+    console.log('color is yellow');
+}
+else{
+    console.log('color is black');
+}
+
+// use of switch
+
+switch(color){
+    case 'blue':
+        console.log('color is blue');
+        break;
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'white':
+        console.log('color is white');
+        break;
+    case 'green':
+        console.log('color is green');
+        break;
+    default:
+        console.log('color is black');
+}
